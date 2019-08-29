5758f8de8ff296e6c3a1a8f4674ebcb93f5e92fd
agustinezequielgomez <agusgomez99@gmail.com>
2019-08-29 00:33:12 -0300

    Pagina frontal actualizada

--- src/app/componentes/principal/principal.component.ts
+++ src/app/componentes/principal/principal.component.ts
@@ -11,6 +11,17 @@ export class PrincipalComponent implements OnInit {
     isFirstOpen: true,
     isFirstDisabled: false
   };
+
+  public content: any[] = [{link: '/Juegos', img: './assets/imagenes/saladejuegos.png', heading: 'Juegos', content: `El método lúdico es un
+   conjunto de estrategias diseñadas para crear un ambiente de armonía en los estudiantes que están inmersos en el proceso de
+    aprendizaje. Este método busca que los alumnos se apropien de los temas impartidos por los docentes utilizando el juego.` },
+    {link: '/Listado', img: './assets/imagenes/listado.jpg', heading: 'Listados de resultados', content: `Los listados de los resultados 
+    con ordenamiento y busqueda`},
+    {link: '/Configuracion', img: './assets/imagenes/Configuracion.png', heading: 'Configuración',
+    content: `Ajustes de la aplicacion y los métodos de autentificación`},
+    {link: '/Jugadores', img: './assets/imagenes/jugadores.png', heading: 'Jugadores',
+    content: `Listado de jugadores`}];
+
   constructor() {  }
 
   ngOnInit() {
